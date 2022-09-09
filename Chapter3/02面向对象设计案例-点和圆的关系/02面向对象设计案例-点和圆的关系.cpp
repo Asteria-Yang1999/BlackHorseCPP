2a2597cbe791a2f1c777a4683669d88968b3d002
Asteria <2252689480@qq.com>
2022-09-09 22:22:11 +0800

    2022.9.9

---
+++ Chapter3/02面向对象设计案例-点和圆的关系/02面向对象设计案例-点和圆的关系.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include "point.h"
+#include "circle.h"
+
+using namespace std;
+
+//设计一个圆形类，和一个点类，计算点和圆的关系，
+//假如圆心坐标为x0，y0，半径为r，点的坐标为x1，y1
+
+
+//利用全局函数来判断点和圆的关系
+void isInCircle(Circle &c, Point &p)
+{
+    //获取圆心和点的距离 的平方
+    int distance = (c.getCenter().getX() - p.getX()) * (c.getCenter().getX() - p.getX()) + (c.getCenter().getY() - p.getY()) * (c.getCenter().getY() - p.getY());
+    int rDistance = c.getR() * c.getR();
+
+    if (rDistance == distance)
+    {
+        cout << "点在圆上" << endl;
+    }
+    else if (rDistance > distance)
+    {
+        cout << "点在圆内" << endl;
+    }
+    else
+    {
+        cout << "点在圆外" << endl;
+    }
+}
+
+void test01()
+{
+    Point p1;
+    p1.setX(10);
+    p1.setY(9);
+
+    Circle c1;
+    Point center;
+    center.setX(10);
+    center.setY(0);
+    c1.setCenter(center);
+    c1.setR(10);
+
+    //利用全局函数判断点和圆的关系
+    isInCircle(c1, p1);
+
+    //利用成员函数来判断点和圆的关系
+    c1.isInCircleByClass(p1);
+}
+
+int main()
+{
+    test01();
+    return 0;
+}
