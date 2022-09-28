7592456b6ad429f5c28a6201a786386c4008e29e
Asteria <2252689480@qq.com>
2022-09-28 11:19:20 +0800

    2022.9.28

---
+++ Chapter7/01静态联编和动态联编/01静态联编和动态联编.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+using namespace std;
+
+class Animal
+{
+public:
+    virtual void speak()
+    {
+        cout << "动物在说话" << endl;
+    }
+
+    virtual void eat()
+    {
+        cout << "动物在吃饭" << endl;
+    }
+};
+
+class Cat :public Animal
+{
+public:
+    void speak()    //可写可不写virtual
+    {
+        cout << "小猫在说话" << endl;
+    }
+
+    virtual void eat()
+    {
+        cout << "小猫在吃鱼" << endl;
+    }
+};
+
+//调用doSpeak speak函数的地址早就绑定好了：早绑定，静态联编，编译阶段就确定好了地址
+//如果想调用cat的speak，就不能提前绑定好函数的地址，所以需要在运行时候再去确定函数的地址
+//动态联编，写法：DoSpeak方法改为虚函数，在母类声明虚函数，发生了多态
+//母类的引用或者指针 指向 子类对象
+void doSpeak(Animal & animal)   //Animal & animal = cat
+{
+    animal.speak();
+}
+
+//如果发生了继承的关系，编译器允许进行类型转换
+void test01()
+{
+    Cat cat;
+    doSpeak(cat);
+}
+
+void test02()
+{
+//    cout << sizeof(Animal) << endl;
+    //母类指针指向子类对象 多态
+    Animal * animal = new Cat;
+
+//    animal->speak();
+
+    //*(int *)*(int *)animal 函数地址
+    ((void(*)()) (*(int *)*(int *)animal))();
+
+
+    ((void(*)()) (*((int *)*(int *)animal + 1)))();
+
+}
+
+int main()
+{
+//    test01();
+    test02();
+    return 0;
+}
---
+++ Chapter7/02多态案例-计算器案例/02多态案例-计算器案例.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+
+using namespace std;
+
+//class Calculator
+//{
+//public:
+//    void setv1(int v)
+//    {
+//        this->val1 = v;
+//    }
+//    void setv2(int v)
+//    {
+//        this->val2 = v;
+//    }
+//
+//    int getResult(string oper)
+//    {
+//        if (oper == "+")
+//        {
+//            return val1 + val2;
+//        }
+//        else if (oper == "-")
+//        {
+//            return val1 - val2;
+//        }
+//    }
+//
+//private:
+//    int val1;
+//    int val2;
+//};
+//
+//void test01()
+//{
+//    Calculator cal;
+//    cal.setv1(10);
+//    cal.setv2(10);
+//    cout << cal.getResult("+") << endl;
+//    cout << cal.getResult("-") << endl;
+//}
+
+//真正的开发中，有个开发原则：开闭原则
+//对扩展开放，对修改关闭
+
+//利用多态实现计算器
+class abstractCalculator
+{
+public:
+    virtual int getResult()
+    {
+        return 0;
+    }
+
+    void setv1(int v)
+    {
+        this->val1 = v;
+    }
+    void setv2(int v)
+    {
+        this->val2 = v;
+    }
+
+public:
+    int val1;
+    int val2;
+};
+
+//加法计算器
+class  PlusCalculator :public abstractCalculator
+{
+public:
+    virtual int getResult()
+    {
+        return val1 + val2;
+    }
+};
+
+class SubCalculator :public abstractCalculator
+{
+public:
+    virtual int getResult()
+    {
+        return val1 - val2;
+    }
+};
+
+class TimesCalculator :public abstractCalculator
+{
+public:
+    virtual int getResult()
+    {
+        return val1 * val2;
+    }
+};
+
+void test01()
+{
+    abstractCalculator * abc;    //加法计算器
+    abc = new PlusCalculator;
+
+    abc->setv1(20);
+    abc->setv2(10);
+
+    cout << abc->getResult() << endl;
+
+    delete abc;
+
+    abc = new SubCalculator;
+    abc->setv1(10);
+    abc->setv2(20);
+    cout << abc->getResult() << endl;
+
+    delete abc;
+
+    abc = new TimesCalculator;
+    abc->setv1(10);
+    abc->setv2(20);
+    cout << abc->getResult() << endl;
+}
+
+int main()
+{
+    test01();
+    return 0;
+}
